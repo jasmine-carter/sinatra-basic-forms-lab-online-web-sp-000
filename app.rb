2d00d07ef54c79b7fa6cab7b3e67fff7fd5a8f62
Jasmine Carter <jasmine.carter87@gmail.com>
2020-03-22 19:19:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,8 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    pup = Puppy.new(params[:name], params[:breed],params[:age])
+    puppy = Puppy.new(params[:name], params[:breed],params[:age])
+
     erb :display_puppy
   end
 end
